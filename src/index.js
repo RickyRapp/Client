e6f917fc3d88e3b611e593838299bfc50ef87742
RickyRapp <105504490+RickyRapp@users.noreply.github.com>
2022-06-14 13:15:34 -0500

    Adding Most Recently Updated Files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import App from './App'; 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux'; 
-import reducers from './reducers';
+import { Provider } from 'react-redux'; 
 import store from './Store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-    <Provider store={store}>
-        <App /> 
-    </Provider>
-    
-);
- 
+ReactDOM.render(    <Provider store={store}>
+    <App /> 
+</Provider>, document.querySelector('#root'));
+  
